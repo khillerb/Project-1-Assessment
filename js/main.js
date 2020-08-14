cf256307e93aa21dc49de218f36b7543a5f53fcf
khillerb <64817143+khillerb@users.noreply.github.com>
2020-08-14 15:49:07 -0500

    Add Everything

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
+const plusCalc = document.getElementById("plusCalc");
+const incrementer = document.getElementById("incrementer")
+const minusCalc = document.getElementById("minusCalc");
+const countatt = document.querySelector('[count-data]');
+let base = 0;
+
+const plusInc = () => {
+	base = parseInt(countatt.innerText) 
+	base += parseInt(incrementer.value)
+	countatt.innerText = base
+	if (base >= 0) {
+		countatt.classList.remove('neg')
+		countatt.classList.add('pos')
+	} else {
+		countatt.classList.remove('pos')
+		countatt.classList.add('neg')
+	}
+}
+const minusInc = () => {
+	base = parseInt(countatt.innerText) 
+	base -= parseInt(incrementer.value)
+	countatt.innerText = base
+	if (base >= 0) {
+		countatt.classList.remove('neg')
+		countatt.classList.add('pos')
+	} else {
+		countatt.classList.remove('pos')
+		countatt.classList.add('neg')
+	}
+}
+plusCalc.addEventListener('click', plusInc);
+minusCalc.addEventListener('click', minusInc);
 
